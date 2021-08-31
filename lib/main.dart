6e1232154c73349490b487be8f1a010e5a3045aa
ejjraifihamza <ejjraifihamza@gmail.com>
2021-08-31 15:47:18 +0100

    🚀 Add Theam

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Login(),
+      theme: ThemeData(
+        primaryColor: Colors.teal[100],
+        textTheme: TextTheme(
+          button: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.teal[100],
+            padding: EdgeInsets.all(20),
+            elevation: 50,
+            shadowColor: Colors.teal[100],
+            textStyle: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+            ),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          ),
+        ),
+      ),
       routes: {
         'login': (context) => Login(),
       },
